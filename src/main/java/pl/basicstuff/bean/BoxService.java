d184bbc10c40cc3d52bbdae31cecbf3a3b21fa27
[Kamil Lazarowicz] <[kamil_lazarowicz@hotmail.com]>
2021-11-07 20:20:14 +0100

    adding future expansion

--- src/main/java/pl/basicstuff/bean/BoxService.java
+++ src/main/java/pl/basicstuff/bean/BoxService.java
@@ -23,12 +23,12 @@ public class BoxService {
         games.add(new Box(8L,"Unmatched: Deadpool (2021)",List.of("Deadpool")));
 
 //        Below expansions are not yet published
-
 //        games.add(new Box(9L,"Hell's Kitchen (2021)",List.of("Daredevil", "Elektra", "Bullseye")));
-//        games.add(new Box(10L,"Battle of Legends, Volume Two (2022)",List.of("Sun Wukong", "Yennenga", "Achilles", "Bloody Mary")));
-//        games.add(new Box(11L,"Redemption Row (2021)",List.of("Luke Cage", "Ghost Rider", "Moon Knight")));
-//        games.add(new Box(12L,"Teen Spirit (2021)",List.of("Ms. Marvel", "Squirrel Girl", "Cloak and Dagger")));
-//        games.add(new Box(13L,"Jurassic Park – Dr. Sattler vs. T. Rex (2021)",List.of("Dr. Sattler", "T.Rex")));
+//        games.add(new Box(10L,"For King and Country (2021)",List.of("Black Widow", "Black Panther", "Winter Soldier")));
+//        games.add(new Box(11L,"Battle of Legends, Volume Two (2022)",List.of("Sun Wukong", "Yennenga", "Achilles", "Bloody Mary")));
+//        games.add(new Box(12L,"Redemption Row (2021)",List.of("Luke Cage", "Ghost Rider", "Moon Knight")));
+//        games.add(new Box(13L,"Teen Spirit (2021)",List.of("Ms. Marvel", "Squirrel Girl", "Cloak and Dagger")));
+//        games.add(new Box(14L,"Jurassic Park – Dr. Sattler vs. T. Rex (2021)",List.of("Dr. Sattler", "T.Rex")));
     }
 
     public List<Box> getList() {
