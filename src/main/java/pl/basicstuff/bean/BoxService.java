094c42c54f17fc2d3d8038b06693d7dcc7b1cd4f
donkichote87 <kamil_lazarowicz@hotmail.com>
2022-05-23 13:46:55 +0200

    fixing small bugs

--- src/main/java/pl/basicstuff/bean/BoxService.java
+++ src/main/java/pl/basicstuff/bean/BoxService.java
@@ -22,12 +22,12 @@ public class BoxService {
         games.add(new Box(5L, "Jurassic Park - InGen vs Raptors (2020)", Arrays.asList("InGen", "Raptors")));
         games.add(new Box(6L, "Buffy the Vampire Slayer (2020)", Arrays.asList("Buffy", "Willow", "Spike", "Angel")));
         games.add(new Box(7L, "Unmatched: Deadpool (2021)", Arrays.asList("Deadpool")));
-        games.add(new Box(8L,"Hell's Kitchen (2021)",List.of("Daredevil", "Elektra", "Bullseye")));
-        games.add(new Box(9L,"For King and Country (2021)",List.of("Black Widow", "Black Panther", "Winter Soldier")));
-        games.add(new Box(10L,"Battle of Legends, Volume Two (2022)",List.of("Sun Wukong", "Yennenga", "Achilles", "Bloody Mary")));
-        games.add(new Box(11L,"Redemption Row (2021)",List.of("Luke Cage", "Ghost Rider", "Moon Knight")));
-        games.add(new Box(12L,"Teen Spirit (2021)",List.of("Ms. Marvel", "Squirrel Girl", "Cloak and Dagger")));
-        games.add(new Box(13L,"Jurassic Park - Dr. Sattler vs. T. Rex (2021)",List.of("Dr. Sattler", "T.Rex")));
+        games.add(new Box(8L, "Hell's Kitchen (2021)", Arrays.asList("Daredevil", "Elektra", "Bullseye")));
+        games.add(new Box(9L, "For King and Country (2021)", Arrays.asList("Black Widow", "Black Panther", "Winter Soldier")));
+        games.add(new Box(10L, "Battle of Legends, Volume Two (2022)", Arrays.asList("Sun Wukong", "Yennenga", "Achilles", "Bloody Mary")));
+        games.add(new Box(11L, "Redemption Row (2021)", Arrays.asList("Luke Cage", "Ghost Rider", "Moon Knight")));
+        games.add(new Box(12L, "Teen Spirit (2021)", Arrays.asList("Ms. Marvel", "Squirrel Girl", "Cloak and Dagger")));
+        games.add(new Box(13L, "Jurassic Park - Dr. Sattler vs. T. Rex (2021)", Arrays.asList("Dr. Sattler", "T.Rex")));
     }
 
     public List<Box> getList() {
